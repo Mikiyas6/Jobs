035ca7038fee465b00d88edc919d8035bea5de76
Mikiyas6 <Mikitedi9@gmail.com>
2024-07-20 18:55:58 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Hero
+        title="Become a React Dev"
+        subtitle="Find the React job that fits your skills and needs"
+      />
+      <HomeCards />
+      <>
+        <JobListings />
+        <ViewAllJobs />
+      </>
+    </>
+  );
 };
 
 export default App;
